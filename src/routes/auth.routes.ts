612b718d708c9c3b5efde1deffa384d195332d9b
Andrii <gaarasandy@gmail.com>
2020-04-24 10:07:01 +0300

    deleted path

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,7 +1,6 @@
 const { Router } = require('express');
 const { authentication } = require('../controllers/auth.controller');
 
-const path = '/login';
 const router = Router();
 
 router.post('login', authentication)
